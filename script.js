ed05e9b02bbb96490124be3c545d8a560c45e20b
Atravias Borley <atravias@gmail.com>
2019-12-19 19:44:27 -0700

    second commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var searchTerm = $("#search-term").val()
+var startYear = $("#start-year").val()
+var endYear = $("#end-year").val()
+
+
+$("search-term").on("click", function () {
+
+})
+
+console.log(searchTerm)
+console.log(startYear)
+console.log(endYear)
